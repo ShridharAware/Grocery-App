67d47520dece8b9aea22c76dd93caa74c2d81c55
Shridhar Aware <ashridhar19@gmail.com>
2023-12-04 19:41:41 +0530

    Fixed bugs in About and FAQ page.

--- Client/src/pages/About/About.tsx
+++ Client/src/pages/About/About.tsx
@@ -1,24 +1,36 @@
-<div className="container mt-5">
-    <div className="row">
-        <div className="col-md-6 offset-md-3">
+const About: React.FC = () => {
+  return (
+    <>
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-6 offset-md-3">
             <h2 className="text-center mb-4">About Us</h2>
 
             <p>
-                Welcome to our Grocery App, where we strive to provide you with the best online shopping experience
-                for all your grocery needs. Our mission is to make grocery shopping convenient, easy, and enjoyable
-                for our customers.
+              Welcome to our Grocery App, where we strive to provide you with
+              the best online shopping experience for all your grocery needs.
+              Our mission is to make grocery shopping convenient, easy, and
+              enjoyable for our customers.
             </p>
 
             <p>
-                Our app offers a wide range of high-quality products, fresh produce, and household essentials. We
-                understand the importance of quality, affordability, and timely delivery, and we are committed to
-                meeting your expectations.
+              Our app offers a wide range of high-quality products, fresh
+              produce, and household essentials. We understand the importance of
+              quality, affordability, and timely delivery, and we are committed
+              to meeting your expectations.
             </p>
 
             <p>
-                If you have any questions, feedback, or suggestions, feel free to reach out to our customer support
-                team. We value your input and are dedicated to continuously improving our services to serve you better.
+              If you have any questions, feedback, or suggestions, feel free to
+              reach out to our customer support team. We value your input and
+              are dedicated to continuously improving our services to serve you
+              better.
             </p>
+          </div>
         </div>
-    </div>
-</div>
+      </div>
+    </>
+  );
+};
+
+export default About;
